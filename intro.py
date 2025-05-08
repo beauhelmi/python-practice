be7c50912e4ad5bfba14baa1425682107db77a08
Beau Helmi <beauhelmi@gmail.com>
2025-05-08 20:35:13 +0800

    Adding all sublime text basic files

---
+++ intro.py
@@ -0,0 +1,13 @@
+message = "Hello World"
+
+print(message[0])
+
+print(message.upper())
+
+print(message.lower())
+
+print(message.find('World'))
+
+new_message = message.replace('World', 'Universe')
+
+print(new_message)
